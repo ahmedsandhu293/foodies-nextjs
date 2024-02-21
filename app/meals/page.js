44f8a8b19dea87f93f921954ddbea60b3707efa0
ahmedsandhu293 <ahmedsandhu293@gmail.com>
2024-02-21 15:50:40 +0500

    basic app complete

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,8 +5,14 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the meals, shared by our vibrant community.",
+};
+
 async function Meals() {
   const meals = await getMeals();
+  //console.log(meals);
   return <MealsGrid meals={meals} />;
 }
 
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,19 +1,12 @@
+"use client";
+import { useFormState } from "react-dom";
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
+import MealFormSubmit from "@/components/meals/meal-form-submit";
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    "use server";
-    const meal = {
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-    };
-    console.log(meal);
-  }
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -23,7 +16,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -51,9 +44,10 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label="Your image" name="name" />
+          <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealFormSubmit />
           </p>
         </form>
       </main>
